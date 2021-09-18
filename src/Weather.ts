16fb05d0d61a270fe8b92963251e626909c92c6b
unknown <nimrod_trb@yahoo.com>
2021-09-18 14:06:35 +0300

    Created Weather interface for data fetching

---
+++ src/Weather.ts
@@ -0,0 +1,18 @@
+export interface Weather {
+    coord : object;
+    baste : string;
+    weather : [];
+    main : object;
+    visibility : number ;
+    wind : object ;
+    clouds : object;
+    dt : number ;
+    sys : object;
+    timezone : number;
+    id : number;
+    name : string;
+    cod : number
+
+
+
+}
